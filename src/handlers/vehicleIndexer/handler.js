70ef4fc49e4acfe6192b1d60ac53db57779bd77c
Alexandrino Santana <alexandrino.santana@gmail.com>
2019-07-13 00:30:01 -0300

    Add vehicle indexer

--- src/handlers/vehicleIndexer/handler.js
+++ src/handlers/vehicleIndexer/handler.js
@@ -1,14 +1,10 @@
-// const vehicleIndexer = require('./vehicleIndexer')
+const vehicleData = require('./vehicleData')
 
-// const API_ENDPOINT = process.env.API_ENDPOINT
-// const APP_ID = process.env.APP_ID
-// const S3_BUCKET = process.env.S3_BUCKET
+module.exports.vehicleDataIndexer = async () => {
+  const { ACCOUNT_ID: accountId } = process.env
+  const account = vehicleData.saveVehicleStats({
+    accountId,
+  })
 
-// module.exports.indexer = (event, context, callback) => {
-//   vehicleIndexer.getVehicles({
-//     API_ENDPOINT,
-//     APP_ID,
-//     S3_BUCKET,
-//   })
-//   callback(null, 'Importing vehicles...')
-// }
+  return account
+}
